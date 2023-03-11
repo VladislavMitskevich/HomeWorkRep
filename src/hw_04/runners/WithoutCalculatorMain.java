4f2dbfe00a057c0e76e93f3d3d664584ce558784
Vlad <mitskevichvlm@gmail.com>
2023-03-11 17:11:35 +0300

    feat: add 1 task WithoutCalculatorMain

---
+++ src/hw_04/runners/WithoutCalculatorMain.java
@@ -0,0 +1,9 @@
+package hw_04.runners;
+
+public class WithoutCalculatorMain {
+    public static void main(String[] args) {
+        double result = 4.1 + 15 * 7 + ((28.0 / 5) * (28.0 / 5));
+
+        System.out.println(result);
+    }
+}
